ef0416d8fd2d9c1a47b188afe204398929b7d4c8
zhangyaowen <zhangyaowenjy@163.com>
2022-09-01 16:21:53 +0800

    下载文件

--- day5/06-下载文件.py
+++ day5/06-下载文件.py
@@ -11,12 +11,12 @@ options.add_experimental_option('prefs', prefs)
 
 # 打开浏览器，请求目标网址
 driver = webdriver.Chrome(options=options)
-driver.get('https://chromedriver.chromium.org/downloads')
+driver.get('https://chromedriver.storage.googleapis.com/index.html?path=105.0.5195.52/')
 
 # 操作：进行下载操作
 time.sleep(6)
 # 定位下载标签，然后点击
-driver.find_element(By.LINK_TEXT, 'ChromeDriver 105.0.5195.52').click()
+driver.find_element(By.LINK_TEXT, 'chromedriver_mac64.zip').click()
 
 # 退出浏览器
 time.sleep(100)
