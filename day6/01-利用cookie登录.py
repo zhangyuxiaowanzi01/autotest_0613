0b7fac42a8804690f8687ec7564cc8e211853889
zhangyaowen <zhangyaowenjy@163.com>
2022-09-02 10:38:35 +0800

    cookie实现登录

---
+++ day6/01-利用cookie登录.py
@@ -0,0 +1,26 @@
+from base import webdriver, By, time
+
+# 打开浏览器， 请求目标网址
+driver = webdriver.Chrome()
+driver.get('https://www.baidu.com')
+
+# TODO 设置cookie，实现登录
+cookies = [
+    {
+        'name': 'BDUSS',
+        'value': 'FmOFdTTGhXYnZNd0plNUt4bHM0akFDWlJnYkFuci1YSUNKdkczWjhVQlk3RGhqSVFBQUFBJCQAAAAAAAAAAAEAAABmRcgnyvOx6rXmZGQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAFhfEWNYXxFjMH'
+    },
+    {
+        'name': 'BDUSS_BFESS',
+        'value': 'FmOFdTTGhXYnZNd0plNUt4bHM0akFDWlJnYkFuci1YSUNKdkczWjhVQlk3RGhqSVFBQUFBJCQAAAAAAAAAAAEAAABmRcgnyvOx6rXmZGQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAFhfEWNYXxFjMH'
+    }
+]
+for cookie in cookies:
+    driver.add_cookie(cookie)
+
+# 浏览器刷新
+driver.refresh()
+
+# 退出浏览器
+time.sleep(10)
+driver.quit()
---
+++ day6/base.py
@@ -0,0 +1,4 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import os
