d4f4e90769086c22fe148e862f202f0f5976a1d2
zhangyaowen <zhangyaowenjy@163.com>
2022-09-04 17:33:52 +0800

    为什么使用unittest

---
+++ day7/03-为什么使用unittest.py
@@ -0,0 +1,16 @@
+import unittest
+
+
+class DemoTestCase(unittest.TestCase):
+    def test01(self):
+        print('test01')
+
+    def test02(self):
+        print('test02')
+
+    def test03(self):
+        print('test03')
+
+
+if __name__ == '__main__':
+    unittest.main()
