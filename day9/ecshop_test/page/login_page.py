f816a51235dea090b4a90748f1df80220f737725
zhangyaowen <zhangyaowenjy@163.com>
2022-09-07 14:30:09 +0800

    login_page

---
+++ day9/ecshop_test/page/login_page.py
@@ -0,0 +1,33 @@
+from common.base import Base, By
+
+
+class LoginPage(Base):
+    def input_username(self, username):
+        """
+        接收用户名输入
+        :param username: 用户名
+        :return:
+        """
+        self.send_keys((By.NAME, 'username'), username)
+
+    def input_password(self, password):
+        """
+        输入密码
+        :param password: 用户密码
+        :return:
+        """
+        self.send_keys((By.NAME, 'password'), password)
+
+    def input_remember(self):
+        """
+        点击保存这次的登录信息
+        :return:
+        """
+        self.click((By.ID, 'remember'))
+
+    def input_submit(self):
+        """
+        点击登录操作
+        :return:
+        """
+        self.click((By.NAME, 'submit'))
