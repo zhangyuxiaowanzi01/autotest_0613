ecb3b5ca0e498ac4dc9d207a5ad3b0cc78a31a19
一张不够花 <112365898+zhangyuxiaowanzi01@users.noreply.github.com>
2023-05-26 13:31:48 +0800

    Update login_case.py
    对自动化中的login进行解释

--- day9/ecshop_test/case/login_case.py
+++ day9/ecshop_test/case/login_case.py
@@ -9,6 +9,9 @@ from common.data_operation import DataOperation
 account_list = DataOperation('account.csv').get_data_to_dict()
 account_list2 = []
 for account in account_list:
+    """
+    读取csv格式中的数据对数据进行分析,使用数据驱动自动化执行
+    """
     try:
         account['password'] = int(account['password'])
     except ValueError:
@@ -19,24 +22,26 @@ print(account_list2)
 
 
 @ddt.ddt
+#  使用数据驱动ddt中的ddt模块(系统自带的优先级,响应速度都是比较高的)
 class LoginCase(unittest.TestCase):
     account_list = account_list
 
     @classmethod
+    #  类方法,执行类的时候自动执行类中的方法
     def setUpClass(cls) -> None:
         # 打开浏览器
         driver = get_driver()
-        cls.login = LoginPage(driver)
-        cls.index = IndexPage(driver)
+        cls.login = LoginPage(driver)  # 初始化对象
+        cls.index = IndexPage(driver)  # 初始化对象
 
     @ddt.data(*account_list)
     def test_login(self, data):
-        username = data.get('username')
-        password = data.get('password')
+        username = data.get('username')  # 读取数据中的账号名
+        password = data.get('password')  # 读取数据中的密码
 
         # 请求目标网址
-        self.login.get(self.login.login_url)
-        self.login.implicitly_wait()
+        self.login.get(self.login.login_url)  # 请求的目标网址
+        self.login.implicitly_wait()  #  使用隐式等待,全局生效
 
         # 登录操作
         self.login.input_username(username)
