d7cf0436348e490a23758b1a8c9e3301174fd80e
一张不够花 <112365898+zhangyuxiaowanzi01@users.noreply.github.com>
2023-05-26 13:19:51 +0800

    Update data_operation.py

--- day9/ecshop_test/common/data_operation.py
+++ day9/ecshop_test/common/data_operation.py
@@ -4,14 +4,20 @@
 import pandas as pd
 import os
 from pprint import pprint
+#  pprint主要是一个格式化打印的输出格式
 
 
 class DataOperation:
     def __init__(self, filename, file_type='csv', sheet_name=0):
         # 读取文件
+        """
+        当类一开始进行创建的时候我们就初始化的执行去读取文集,使用系统只带的os模块对文件的相对路径进行查询读取
+        """
         base_path = os.path.dirname(os.path.dirname(__file__))
         file_path = os.path.join(base_path, 'data', filename)
-
+        """
+        这里主要是考虑一个健壮性,一个csv文件中坑你会存在一个第二页的情况,使用if语句进行判断
+        """
         if file_type == 'csv':
             self.data = pd.read_csv(file_path)
         else:
